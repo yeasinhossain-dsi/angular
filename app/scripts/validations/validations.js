ca3261c3876be45615dd8cde4029c108ef4f007b
Yeasin <yeasinhossain-dsi@gmail.com>
2014-11-13 19:39:47 +0600

    custom validation added

--- app/scripts/validations/validations.js
+++ app/scripts/validations/validations.js
@@ -1,8 +1,23 @@
-app.directive('foulPattern', function(){
+app.directive('badword', function(){
     
     return {
         require: 'ngModel',
-        
+        link: function(scope, elm, attrs, ctrl){
+            
+            var filterWords = ['fuck', 'dick'];
+            
+            ctrl.$validators.badword = function(){
+                
+                var elemVal = elm.val().split(' ');
+                
+                if( elemVal.toLowerCase() in filterWords )
+                    
+                    return false;
+                
+                return true;
+                
+            }
+        }
     };
     
 });
